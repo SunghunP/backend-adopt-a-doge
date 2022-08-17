72eb7873fbe79b988178730b947a118507164696
Joe Park <sunghun.josephp@gmail.com>
2022-08-17 12:59:17 -0700

    Create server side update functionality

---
+++ controllers/DogeCtrl/Update.js
@@ -0,0 +1,14 @@
+const Doge = require('../../models/doge');
+
+module.exports = {
+  update
+};
+
+async function update(req, res) {
+  try {
+    await Doge.findByIdAndUpdate(req.params.id, req.body);
+    res.redirect("/doge");
+  } catch (err) {
+    res.status(404).json(err);
+  }
+};
--- routes/dogeRouter.js
+++ routes/dogeRouter.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const IndexCtrl = require('../controllers/DogeCtrl/Index');
 const CreateCtrl = require('../controllers/DogeCtrl/Create');
+const UpdateCtrl = require('../controllers/DogeCtrl/Update')
 
 router.get('/', IndexCtrl.index);
 router.post('/', CreateCtrl.create);
+router.put('/:id', UpdateCtrl.update);
 
 module.exports = router;
