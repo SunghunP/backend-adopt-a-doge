148a899d195eafaebf0d1840b7019175a14bef66
Joe Park <sunghun.josephp@gmail.com>
2022-08-17 11:16:01 -0700

    Init commit

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+require('./connection/db');
+
+// Dependencies
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+const PORT = process.env.PORT || 3232;
+
+app.use(cors());
+app.use(express.json());
+
+const dogeRouter = require('./routes/dogeRouter');
+app.use('/doge', dogeRouter);
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port: ${PORT}`)
+})
