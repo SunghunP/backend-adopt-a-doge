56cda17a6743b689393c157b724a8605161f7d41
Joe Park <sunghun.josephp@gmail.com>
2022-08-17 17:11:17 -0700

    Add test route for heroku

--- server.js
+++ server.js
@@ -11,6 +11,11 @@ const PORT = process.env.PORT || 5001;
 app.use(cors());
 app.use(express.json());
 
+// test route
+app.get("/", (req, res)=> {
+  res.send("Home page for Doge")
+})
+
 const dogeRouter = require('./routes/dogeRouter');
 app.use('/doge', dogeRouter);
 
