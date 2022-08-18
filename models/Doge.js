8ed18b56e16b0ceac486529c0d6e438d63faaaa0
Joe Park <sunghun.josephp@gmail.com>
2022-08-17 17:59:45 -0700

    Change doge schema

--- models/Doge.js
+++ models/Doge.js
@@ -3,10 +3,7 @@ const { Schema, model } = mongoose;
 
 const dogeSchema = new Schema({
   name: { type: String, required: true },
-  id: { type: Number, required: true },
   breed: { type: String, required: true },
-  height: { type: Number, required: true },
-  weight: { type: Number, required: true },
   desc: { type: String, maxLength: 500 },
   adopted: { type: Boolean, default: false }
 });
